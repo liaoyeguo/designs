1480326eb9cdd140432bea0d21249a3f1d81ee0d
liaoyeguo <liaoyeguo@163.com>
2023-03-27 09:51:27 +0000

    Update 4 files

--- components/WhiteBoard/index.tsx
+++ components/WhiteBoard/index.tsx
@@ -2,14 +2,16 @@ import { FC, CSSProperties } from "react";
 import styles from "./index.module.css";
 interface Props {
   background?: CSSProperties["background"];
+  full?: boolean;
 }
 const WhiteBoard: FC<Props> = (props) => {
-  const { background, children } = props;
+  const { background, children, full } = props;
 
   return (
     <div
       className={styles.wrapper}
       style={{
+        padding: full ? 0 : undefined,
         background,
       }}
     >
